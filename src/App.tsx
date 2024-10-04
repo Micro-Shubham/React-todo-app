190d77897b8002e06bcab21685154bc54d765c66
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-10-04 12:48:41 +0530

    rendering items

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,45 @@
-import './App.css'
+import "./App.css";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
+  let nextId = 0;
   // let item = "";
-  let array =['a','b','c','']
-  const [Value, setValue] = useState("");
+  const [value, setValue] = useState("");
+  const [items, setItems]  = useState<{id:number ;text:string}[]>([]);
 
   return (
     <>
-     <h1>ToDo App</h1>
-     <input placeholder='do somthing....' type='text'value={Value}  onChange={(e) => setValue(e.target.value)} ></input>
- {/* <Input ></Input> */}
-    {/* <Button></Button> */}
-    <button id="btn" onClick={(event) => { console.log(Value);setValue("");console.log(event)}} >Click me!</button>
+      <h1>ToDo App</h1>
+      <input
+        placeholder="do somthing...."
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      ></input>
+      {/* <Input ></Input> */}
+      {/* <Button></Button> */}
+      <button
+        id="btn"
+        onClick={() => {
+          if(value !="") {
+            setItems((prevItems) =>
+            prevItems.concat({id:nextId, text:value}) 
+            );
+          }
+          setValue("");
+        }}
+      >
+        Click me!
+      </button>
+      {/* Render  the list  of items  */}
+      <ul>
+        {items.map((item) =>(
+          <li key={item.id}>{item.text}</li> //Display  each items text
+        ))}
+      </ul>
     </>
-  )
-
+  );
 }
 
-export default App
+export default App;
